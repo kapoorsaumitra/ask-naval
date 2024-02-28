1dc7b1c5897c02051c6b9e7b751c67a96a3654f3
kapoorsaumitra <saumitrakapoor24@gmail.com>
2024-02-28 14:52:31 +0530

    starter commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import { Hero } from './components/Hero'
+import { DotBackgroundDemo } from './components/DotBackground'
+
+function App() {
+
+  return (
+    <>
+      {/* <DotBackgroundDemo></DotBackgroundDemo> */}
+      <Hero></Hero>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Hero.jsx
@@ -0,0 +1,10 @@
+export function Hero(){
+    return(
+        <>
+            <div style={{width: '100%', height: '100%', paddingTop: 208, paddingBottom: 476, paddingLeft: 71, paddingRight: 71, background: 'white', flexDirection: 'column', justifyContent: 'flex-start', alignItems: 'center', display: 'inline-flex'}}>
+            <img style={{width: 284, height: 267}} src="./src/assets/naval.jpg" />
+            <input type="text" style={{width: 1298, height: 73, background: 'rgba(65, 65, 65, 0.39)', boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.25) inset', borderRadius: 35}} />
+            </div>
+        </>
+    )
+}
