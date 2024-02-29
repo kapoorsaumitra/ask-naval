50b6116a55a615cecd9c5ecaef8cf871abaa9794
kapoorsaumitra <saumitrakapoor24@gmail.com>
2024-02-29 22:38:13 +0530

    image fox

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,13 @@
+import navallogo from './src/assets/naval.jpg'
+
 export function Hero(){
     return(
         <>
             <div style={{width: '100%', height: '100%', paddingTop: 208, paddingBottom: 476, paddingLeft: 71, paddingRight: 71, background: 'white', flexDirection: 'column', justifyContent: 'flex-start', alignItems: 'center', display: 'inline-flex'}}>
             <img style={{width: 284, height: 267}} src="./src/assets/naval2.JPG" />
-            <input type="text" style={{width: 1298, height: 73, background: 'rgba(65, 65, 65, 0.39)', boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.25) inset', borderRadius: 35}} />
+            <input type="text" className=" w-full h-12 bg-neutral-700 rounded-[35px] shadow"/>
+            
+            
             </div>
         </>
     )
