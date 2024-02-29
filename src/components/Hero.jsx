3132c35013af71f7405becdbe2215e49fa6787c6
kapoorsaumitra <saumitrakapoor24@gmail.com>
2024-02-29 22:38:51 +0530

    image fix

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,7 +4,7 @@ export function Hero(){
     return(
         <>
             <div style={{width: '100%', height: '100%', paddingTop: 208, paddingBottom: 476, paddingLeft: 71, paddingRight: 71, background: 'white', flexDirection: 'column', justifyContent: 'flex-start', alignItems: 'center', display: 'inline-flex'}}>
-            <img style={{width: 284, height: 267}} src="./src/assets/naval2.JPG" />
+            <img style={{width: 284, height: 267}} src={navallogo} />
             <input type="text" className=" w-full h-12 bg-neutral-700 rounded-[35px] shadow"/>
             
             
