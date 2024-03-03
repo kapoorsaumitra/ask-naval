b947a27c83bbc08dec7df601c7f43ddbcac33544
rishabhknowss <rishabhrai1515@gmail.com>
2024-03-03 19:45:38 +0530

    changed logo transition

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,6 +5,13 @@ const pathVariants = {
     hidden: {
         pathLength: 0,
     },
+    fast : {
+        pathLength: 1,
+        transition: {
+            duration: 0.5,
+            ease: "easeInOut",
+        },
+    },
     visible: {
         pathLength: 1,
         transition: {
@@ -40,13 +47,13 @@ export function Hero(){
                 <motion.path variants={pathVariants} initial="hidden" animate="visibleFast" d="M96 653.5V525.5" stroke="black" strokeWidth="16"/>
                 <motion.path variants={pathVariants} initial="hidden" animate="visibleFast" d="M368 654V526" stroke="black" strokeWidth="16"/>
                 {/* mouth */}
-                <motion.path variants={pathVariants} initial="hidden" animate="visible" d="M175.5 473.5C198 469.667 252.7 464.3 291.5 473.5" stroke="black" strokeWidth="16"/>
+                <motion.path variants={pathVariants} initial="hidden" animate="fast" d="M175.5 473.5C198 469.667 252.7 464.3 291.5 473.5" stroke="black" strokeWidth="16"/>
                 {/* chin */}
-                <motion.path variants={pathVariants} initial="hidden" animate="visible" d="M263 518.5H207.5" stroke="black" strokeWidth="16"/>
+                <motion.path variants={pathVariants} initial="hidden" animate="fast" d="M263 518.5H207.5" stroke="black" strokeWidth="16"/>
                 {/* shakal support horizontal */}
                 <motion.path variants={pathVariants} initial="hidden" animate="visibleFaster" d="M2.5 281.5C86 265 294.4 241.9 460 281.5" stroke="black" strokeWidth="16"/>
                 {/* nose */}
-                <motion.path variants={pathVariants} initial="hidden" animate="visible" d="M207.5 419.5H263" stroke="black" strokeWidth="16"/>
+                <motion.path variants={pathVariants} initial="hidden" animate="fast" d="M207.5 419.5H263" stroke="black" strokeWidth="16"/>
             </motion.svg>
 
             <input type="text" className=" w-96 h-12 bg-neutral-700 rounded-[35px] shadow-2xl text-white"/>
